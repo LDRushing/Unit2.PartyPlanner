0039f453f6b1728088d2b9241e595431a34a9785
LDRushing <rushinglucy@yahoo.com>
2023-11-02 19:29:24 -0500

    final draft

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ async function fetchAndRenderParties() {
     const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events');
     const parties = await response.json();
     const partyList = document.getElementById('party-list');
-
+    console.log("This is parties", parties);
     partyList.innerHTML = ''; // Clear existing list
-
-    parties.forEach(party => {
+//parties.data lets is see the API we're tied to. When we make a fetch request, the response is usually an object with elements associated with it. 
+//console.log things to see what's going on in your code.
+    parties.data.forEach(party => {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
             <b>Name:</b> ${party.name}</br>
@@ -24,23 +25,24 @@ async function fetchAndRenderParties() {
 // Event listener for the party form submission
 document.getElementById('party-form').addEventListener('submit', async (event) => {
     event.preventDefault();
-
+//Invoke methods with parenthesis. 
     const name = document.getElementById('name').value;
-    const date = document.getElementById('date').value;
+    const date = new Date(document.getElementById('date').value).toISOString();
     const time = document.getElementById('time').value;
     const location = document.getElementById('location').value;
     const description = document.getElementById('description').value;
-
+//Catch is meant to catch exceptional errors like a lack of wifi. 
     const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name, date, time, location, description }),
+        body: JSON.stringify({ name, date, location, description }),
     });
 
     if (response.status === 201) {
         fetchAndRenderParties(); // Refresh the list after adding a new party
+        return response;
     }
 
     document.getElementById('party-form').reset(); // Reset the form
@@ -59,4 +61,5 @@ document.getElementById('party-list').addEventListener('click', async (event) =>
         }
     }
 });
+console.log("This is before fetchAndRenderParties")
 fetchAndRenderParties();
